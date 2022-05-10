e471813fd54225e9e8fa3f58e19b220666d3ce1a
Victor Inojosa <vijoin@gmail.com>
2022-05-10 11:16:57 -0300

    Add autoincremental token ID (with test)

--- contracts/AlchemyNFT.sol
+++ contracts/AlchemyNFT.sol
@@ -3,11 +3,18 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract AlchemyNFT is ERC721 {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
     constructor() ERC721("Alchemy", "ALCH") {}
 
-    function safeMint(address to, uint tokenId) public {
+    function safeMint(address to) public {
+        uint tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 }
--- test/AlchemyNFT.js
+++ test/AlchemyNFT.js
@@ -6,14 +6,22 @@ describe("Alchemy NFT Contract", () => {
     beforeEach( async () => {
 
         Alchemy = await ethers.getContractFactory("AlchemyNFT");
-        [owner, ...addrs] = await ethers.getSigners();
+        [owner, addr1, ...addrs] = await ethers.getSigners();
         alchemy = await Alchemy.deploy();
     });
     
     describe.only("Transactions", () => {    
         it("Test minting", async () => {
-            await alchemy.safeMint(owner.address, Math.floor(Math.random() * 100))
+            // Mint first NFT
+            await alchemy.safeMint(owner.address)
             expect(await alchemy.balanceOf(owner.address)).to.be.equal(1)
+            expect(await alchemy.ownerOf(0)).to.be.equal(owner.address)
+
+            // Mint second NFT
+            await alchemy.safeMint(addr1.address)
+            expect(await alchemy.balanceOf(addr1.address)).to.be.equal(1)
+            expect(await alchemy.ownerOf(1)).to.be.equal(addr1.address)
+
         });
     });
 
