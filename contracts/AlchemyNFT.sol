a64b9354cb778f65df4be5a24e9e5b231007cfc6
Victor Inojosa <vijoin@gmail.com>
2022-05-10 23:26:23 -0300

    Add enumerable capabilities (with tests)

--- contracts/AlchemyNFT.sol
+++ contracts/AlchemyNFT.sol
@@ -3,9 +3,10 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract AlchemyNFT is ERC721 {
+contract AlchemyNFT is ERC721, ERC721Enumerable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -17,4 +18,21 @@ contract AlchemyNFT is ERC721 {
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
+
+    // overrides required by Solidity
+    function _beforeTokenTransfer(address from, address to, uint tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);   
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+     {
+        return super.supportsInterface(interfaceId);
+     }
 }
--- test/AlchemyNFT.js
+++ test/AlchemyNFT.js
@@ -1,28 +1,52 @@
 const { expect } = require("chai");
+const { BigNumber } = require("ethers");
 const { ethers } = require("hardhat");
 
 describe("Alchemy NFT Contract", () => {
+  beforeEach(async () => {
+    Alchemy = await ethers.getContractFactory("AlchemyNFT");
+    [owner, addr1, ...addrs] = await ethers.getSigners();
+    alchemy = await Alchemy.deploy();
+  });
 
-    beforeEach( async () => {
+  describe("Transactions", () => {
+    it("Test minting", async () => {
+      // Mint first NFT
+      await alchemy.safeMint(owner.address);
+      expect(await alchemy.balanceOf(owner.address)).to.be.equal(1);
+      expect(await alchemy.ownerOf(0)).to.be.equal(owner.address);
 
-        Alchemy = await ethers.getContractFactory("AlchemyNFT");
-        [owner, addr1, ...addrs] = await ethers.getSigners();
-        alchemy = await Alchemy.deploy();
+      // Mint second NFT
+      await alchemy.safeMint(addr1.address);
+      expect(await alchemy.balanceOf(addr1.address)).to.be.equal(1);
+      expect(await alchemy.ownerOf(1)).to.be.equal(addr1.address);
     });
-    
-    describe.only("Transactions", () => {    
-        it("Test minting", async () => {
-            // Mint first NFT
-            await alchemy.safeMint(owner.address)
-            expect(await alchemy.balanceOf(owner.address)).to.be.equal(1)
-            expect(await alchemy.ownerOf(0)).to.be.equal(owner.address)
+  });
 
-            // Mint second NFT
-            await alchemy.safeMint(addr1.address)
-            expect(await alchemy.balanceOf(addr1.address)).to.be.equal(1)
-            expect(await alchemy.ownerOf(1)).to.be.equal(addr1.address)
-
-        });
+  describe("Enumerable capabilities", () => {
+    it("Count all NFTs minted", async () => {
+      const minted_count = Math.floor(Math.random() * 100);
+      for (let i = 0; i < minted_count; i++) {
+        await alchemy.safeMint(owner.address);
+      }
+      expect(await alchemy.totalSupply()).to.be.equal(minted_count);
     });
 
+    it("List all NFTs of owner", async () => {
+      for (let i = 0; i < 3; i++) {
+        await alchemy.safeMint(owner.address);
+        await alchemy.safeMint(addr1.address);
+      }
+
+      const totalNFTsOwner = await alchemy.balanceOf(owner.address);
+
+      let listOfTokensOfOwner = new Array();
+      for (let i = 0; i < totalNFTsOwner; i++) {
+        listOfTokensOfOwner.push(
+          (await alchemy.tokenOfOwnerByIndex(owner.address, i)).toNumber()
+        );
+      }
+      expect(listOfTokensOfOwner).to.eql([0, 2, 4]);
+    });
+  });
 });
