971010829b62a4b0148bb352aa2c4b698567aebf
Victor Inojosa <vijoin@gmail.com>
2022-05-10 23:36:15 -0300

    Add ERC721 URI Storage capabilities

--- contracts/AlchemyNFT.sol
+++ contracts/AlchemyNFT.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract AlchemyNFT is ERC721, ERC721Enumerable {
+contract AlchemyNFT is ERC721, ERC721Enumerable, ERC721URIStorage {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -14,17 +15,34 @@ contract AlchemyNFT is ERC721, ERC721Enumerable {
     constructor() ERC721("Alchemy", "ALCH") {}
 
     function safeMint(address to) public {
-        uint tokenId = _tokenIdCounter.current();
+        uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 
     // overrides required by Solidity
-    function _beforeTokenTransfer(address from, address to, uint tokenId)
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId)
         internal
-        override(ERC721, ERC721Enumerable)
+        override(ERC721, ERC721URIStorage)
     {
-        super._beforeTokenTransfer(from, to, tokenId);   
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 
     function supportsInterface(bytes4 interfaceId)
@@ -32,7 +50,7 @@ contract AlchemyNFT is ERC721, ERC721Enumerable {
         view
         override(ERC721, ERC721Enumerable)
         returns (bool)
-     {
+    {
         return super.supportsInterface(interfaceId);
-     }
+    }
 }
